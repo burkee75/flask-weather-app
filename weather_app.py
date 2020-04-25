3f84165a8bb4bd32f62c2496a214f8dbd69ca9c4
Brad Burkett <bradburkett75@gmail.com>
2020-04-25 08:02:20 -0600

    moved NOAA weather request to class in noaa.py

--- weather_app.py
+++ weather_app.py
@@ -7,6 +7,7 @@ from requests.exceptions import HTTPError, Timeout
 import sys 
 import yaml
 from location import Location
+from noaa import NoaaWeather
 
 
 app = Flask(__name__)
@@ -34,39 +35,14 @@ def weather():
         coordinates = f"{str(location.latitude_longitude(zipcode)[1])},{str(location.latitude_longitude(zipcode)[0])}"
         print(f'Formatted coordinates are: {coordinates}') #for degbugging
 
-        # First get forecast zone, then get forecast
+        # Get weather forecast
+        weather = NoaaWeather(coordinates) #returns json
+        #print(weather.get_weather_forecast())
 
-        headers = {
-            'User-Agent':'personal-project'
-            }
-        try:
-            zone_url = requests.get(f'https://api.weather.gov/points/{coordinates}', headers=headers, timeout=5)
-            zone_url.raise_for_status()
-            print(f"Weather Request HTTP Code: {zone_url.status_code}") 
-        except Timeout:
-            print("The request timed out...")
-        except HTTPError as http_err:
-            print(f'HTTP error occurred: {http_err}') 
-        except Exception as err:
-            print(f'Other error occurred: {err}')
-            sys.exit(1)   
-        forecast_request = zone_url.json()['properties']['forecast']
-
-        try:
-            forecast = requests.get(forecast_request, headers=headers, timeout=5)
-            forecast.raise_for_status()
-            print(f"Weather Request HTTP Code: {forecast.status_code}") 
-        except Timeout:
-            print("The request timed out...")
-        except HTTPError as http_err:
-            print(f'HTTP error occurred: {http_err}') 
-        except Exception as err:
-            print(f'Other error occurred: {err}')
-            sys.exit(1)   
-
-        tomorrow_summary = forecast.json()['properties']['periods'][2]['shortForecast']
+        tomorrow_summary = weather.get_weather_forecast()['properties']['periods'][2]['shortForecast']
         print(f"Tomorrow's Forecast is: {tomorrow_summary}")
-        return render_template("weather.html", result = tomorrow_summary)
+
+        return render_template("weather.html", result = tomorrow_summary())
 
 
 if __name__ == '__main__':
